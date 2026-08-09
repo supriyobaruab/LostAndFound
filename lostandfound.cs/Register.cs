13f95503b72c15163518995799ece8cdb3179558
supriyobaruab <suxpriyo@gmail.com>
2026-08-09 08:55:30 -0700

    feat: Add User Validation before Registration

--- lostandfound.cs/Register.cs
+++ lostandfound.cs/Register.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -38,7 +39,7 @@ namespace lostandfound.cs
                 MessageBox.Show("Please fill in all fields.");
                 return;
             }
-            if (!Regex.IsMatch(Txt_UserID.Text, @"^[a-zA-Z0-9_]+$"))
+            if (!Regex.IsMatch(Txt_UserID.Text, @"^[a-zA-Z0-9_-]+$"))
             {
                 MessageBox.Show("User ID can only contain letters, numbers, and underscores.");
                 return;
@@ -57,9 +58,48 @@ namespace lostandfound.cs
                     MessageBox.Show("Passwords do not match.");
                     return;
                 }
-                else 
+                else
                 {
-                    //Database Logic 
+                    string connectionString = "Server=localhost;Database=LostAndFound;Trusted_Connection=true; TrustServerCertificate=true;";
+                    SqlConnection conn = new SqlConnection(connectionString);
+
+                    try
+                    {
+                        conn.Open();
+                        //Check User
+                        string queryCheck = "SELECT COUNT(*) FROM [User] WHERE User_ID = @User_ID";
+                        SqlCommand cmdCheck = new SqlCommand(queryCheck, conn);
+
+                        cmdCheck.Parameters.AddWithValue("@User_ID", Txt_UserID.Text);
+                        
+
+                        int count = (int)cmdCheck.ExecuteScalar();
+                        if (count > 0)
+                        {
+                            MessageBox.Show("User ID already exists. Please reset your Password.");
+                            return;
+                        }
+
+                        //Insert User
+                        string query = "INSERT INTO [User] (Name, Email, User_ID, Password, Role) VALUES (@Name, @Email, @User_ID, @Password, @Role)";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@Name", TxtUser.Text);
+                        cmd.Parameters.AddWithValue("@Email", TxtEmail.Text);
+                        cmd.Parameters.AddWithValue("@User_ID", Txt_UserID.Text);
+                        cmd.Parameters.AddWithValue("@Password", Txt_Password.Text);
+                        cmd.Parameters.AddWithValue("@Role", RD_Student.Checked ? "student" : "officer");
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Registration successful!");
+                    }
+                    catch(Exception Error) {
+                        MessageBox.Show(Error.Message);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                        this.Close();
+                    }
                 }
             }
             else
