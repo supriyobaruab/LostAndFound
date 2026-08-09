55d1b40661edd3c098f3ca287ba6ebcbb07803bd
Sujoy Barua <sujoybinary@gmail.com>
2026-08-10 04:21:09 +0600

    Added user table view and added columns

--- lostandfound.cs/AdminDashboard.Designer.cs
+++ lostandfound.cs/AdminDashboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.LB_User = new System.Windows.Forms.Label();
             this.PanelHeader = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -35,7 +36,15 @@
             this.LB_LFHeader = new System.Windows.Forms.Label();
             this.LB_AdminP = new System.Windows.Forms.Label();
             this.btn_Logout = new System.Windows.Forms.Button();
+            this.LB_rUsers = new System.Windows.Forms.Label();
+            this.btn_AddUser = new System.Windows.Forms.Button();
+            this.Grd_User = new System.Windows.Forms.DataGridView();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRole = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAction = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PanelHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grd_User)).BeginInit();
             this.SuspendLayout();
             // 
             // LB_User
@@ -118,21 +127,89 @@
             this.btn_Logout.UseVisualStyleBackColor = false;
             this.btn_Logout.Click += new System.EventHandler(this.btn_Logout_Click);
             // 
+            // LB_rUsers
+            // 
+            this.LB_rUsers.AutoSize = true;
+            this.LB_rUsers.Location = new System.Drawing.Point(55, 108);
+            this.LB_rUsers.Name = "LB_rUsers";
+            this.LB_rUsers.Size = new System.Drawing.Size(90, 13);
+            this.LB_rUsers.TabIndex = 5;
+            this.LB_rUsers.Text = "4 registered users";
+            // 
+            // btn_AddUser
+            // 
+            this.btn_AddUser.Location = new System.Drawing.Point(492, 98);
+            this.btn_AddUser.Name = "btn_AddUser";
+            this.btn_AddUser.Size = new System.Drawing.Size(75, 23);
+            this.btn_AddUser.TabIndex = 6;
+            this.btn_AddUser.Text = "Add User";
+            this.btn_AddUser.UseVisualStyleBackColor = true;
+            // 
+            // Grd_User
+            // 
+            this.Grd_User.AllowUserToResizeRows = false;
+            this.Grd_User.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.Grd_User.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grd_User.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colName,
+            this.colRole,
+            this.colEmail,
+            this.colAction});
+            this.Grd_User.EnableHeadersVisualStyles = false;
+            this.Grd_User.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(242)))));
+            this.Grd_User.Location = new System.Drawing.Point(58, 134);
+            this.Grd_User.Name = "Grd_User";
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(43)))), ((int)(((byte)(77)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(245)))), ((int)(((byte)(249)))));
+            this.Grd_User.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.Grd_User.Size = new System.Drawing.Size(509, 144);
+            this.Grd_User.TabIndex = 7;
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            this.colName.Width = 90;
+            // 
+            // colRole
+            // 
+            this.colRole.HeaderText = "Role";
+            this.colRole.Name = "colRole";
+            this.colRole.Width = 80;
+            // 
+            // colEmail
+            // 
+            this.colEmail.HeaderText = "Email";
+            this.colEmail.Name = "colEmail";
+            this.colEmail.Width = 150;
+            // 
+            // colAction
+            // 
+            this.colAction.HeaderText = "Action";
+            this.colAction.Name = "colAction";
+            this.colAction.Width = 150;
+            // 
             // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Grd_User);
+            this.Controls.Add(this.btn_AddUser);
+            this.Controls.Add(this.LB_rUsers);
             this.Controls.Add(this.LogoHeader);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.PanelHeader);
             this.Controls.Add(this.LB_User);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "AdminDashboard";
             this.RightToLeftLayout = true;
             this.Text = "AdminDashboard";
             this.PanelHeader.ResumeLayout(false);
             this.PanelHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grd_User)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,5 +224,12 @@
         private System.Windows.Forms.Label LB_LFHeader;
         private System.Windows.Forms.Label LB_AdminP;
         private System.Windows.Forms.Button btn_Logout;
+        private System.Windows.Forms.Label LB_rUsers;
+        private System.Windows.Forms.Button btn_AddUser;
+        private System.Windows.Forms.DataGridView Grd_User;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRole;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAction;
     }
 }
