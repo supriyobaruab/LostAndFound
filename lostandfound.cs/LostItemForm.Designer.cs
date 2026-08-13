c946b9db38d29cc783838a0820758bae9b239640
Sujoy Barua <sujoybinary@gmail.com>
2026-08-14 02:47:31 +0600

    Resize nowrins merge conflict

--- lostandfound.cs/LostItemForm.Designer.cs
+++ lostandfound.cs/LostItemForm.Designer.cs
@@ -46,10 +46,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(364, 28);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(199, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 25);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Report Lost Item";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -58,7 +58,7 @@
             // 
             this.LB_ItemName.AutoSize = true;
             this.LB_ItemName.Location = new System.Drawing.Point(115, 86);
-            this.LB_ItemName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_ItemName.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.LB_ItemName.Name = "LB_ItemName";
             this.LB_ItemName.Size = new System.Drawing.Size(58, 13);
             this.LB_ItemName.TabIndex = 1;
@@ -68,8 +68,8 @@
             // LB_Catagory
             // 
             this.LB_Catagory.AutoSize = true;
-            this.LB_Catagory.Location = new System.Drawing.Point(464, 89);
-            this.LB_Catagory.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_Catagory.Location = new System.Drawing.Point(464, 86);
+            this.LB_Catagory.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.LB_Catagory.Name = "LB_Catagory";
             this.LB_Catagory.Size = new System.Drawing.Size(49, 13);
             this.LB_Catagory.TabIndex = 2;
@@ -80,7 +80,7 @@
             // 
             this.LB_Description.AutoSize = true;
             this.LB_Description.Location = new System.Drawing.Point(115, 160);
-            this.LB_Description.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_Description.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.LB_Description.Name = "LB_Description";
             this.LB_Description.Size = new System.Drawing.Size(60, 13);
             this.LB_Description.TabIndex = 3;
@@ -91,7 +91,7 @@
             // 
             this.LB_LostLoc.AutoSize = true;
             this.LB_LostLoc.Location = new System.Drawing.Point(115, 307);
-            this.LB_LostLoc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_LostLoc.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.LB_LostLoc.Name = "LB_LostLoc";
             this.LB_LostLoc.Size = new System.Drawing.Size(71, 13);
             this.LB_LostLoc.TabIndex = 4;
@@ -100,17 +100,17 @@
             // 
             // TB_ItemName
             // 
-            this.TB_ItemName.Location = new System.Drawing.Point(203, 83);
-            this.TB_ItemName.Margin = new System.Windows.Forms.Padding(2);
+            this.TB_ItemName.Location = new System.Drawing.Point(202, 79);
+            this.TB_ItemName.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TB_ItemName.Name = "TB_ItemName";
-            this.TB_ItemName.Size = new System.Drawing.Size(229, 20);
+            this.TB_ItemName.Size = new System.Drawing.Size(210, 20);
             this.TB_ItemName.TabIndex = 5;
             this.TB_ItemName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // TB_Description
             // 
-            this.TB_Description.Location = new System.Drawing.Point(203, 157);
-            this.TB_Description.Margin = new System.Windows.Forms.Padding(2);
+            this.TB_Description.Location = new System.Drawing.Point(202, 157);
+            this.TB_Description.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TB_Description.Multiline = true;
             this.TB_Description.Name = "TB_Description";
             this.TB_Description.Size = new System.Drawing.Size(489, 104);
@@ -119,18 +119,18 @@
             // 
             // TB_LostLoc
             // 
-            this.TB_LostLoc.Location = new System.Drawing.Point(203, 304);
-            this.TB_LostLoc.Margin = new System.Windows.Forms.Padding(2);
+            this.TB_LostLoc.Location = new System.Drawing.Point(198, 300);
+            this.TB_LostLoc.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.TB_LostLoc.Name = "TB_LostLoc";
-            this.TB_LostLoc.Size = new System.Drawing.Size(229, 20);
+            this.TB_LostLoc.Size = new System.Drawing.Size(214, 20);
             this.TB_LostLoc.TabIndex = 8;
             this.TB_LostLoc.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // LB_LostDate
             // 
             this.LB_LostDate.AutoSize = true;
-            this.LB_LostDate.Location = new System.Drawing.Point(460, 307);
-            this.LB_LostDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LB_LostDate.Location = new System.Drawing.Point(464, 300);
+            this.LB_LostDate.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.LB_LostDate.Name = "LB_LostDate";
             this.LB_LostDate.Size = new System.Drawing.Size(53, 13);
             this.LB_LostDate.TabIndex = 9;
@@ -139,8 +139,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 383);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(107, 387);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 38);
             this.button1.TabIndex = 11;
@@ -150,8 +150,8 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(206, 383);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(198, 387);
+            this.button2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(84, 38);
             this.button2.TabIndex = 12;
@@ -162,8 +162,8 @@
             // CB_Catagory
             // 
             this.CB_Catagory.FormattingEnabled = true;
-            this.CB_Catagory.Location = new System.Drawing.Point(547, 86);
-            this.CB_Catagory.Margin = new System.Windows.Forms.Padding(2);
+            this.CB_Catagory.Location = new System.Drawing.Point(546, 79);
+            this.CB_Catagory.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.CB_Catagory.Name = "CB_Catagory";
             this.CB_Catagory.Size = new System.Drawing.Size(145, 21);
             this.CB_Catagory.TabIndex = 13;
@@ -171,8 +171,8 @@
             // 
             // DTP_LostDate
             // 
-            this.DTP_LostDate.Location = new System.Drawing.Point(547, 304);
-            this.DTP_LostDate.Margin = new System.Windows.Forms.Padding(2);
+            this.DTP_LostDate.Location = new System.Drawing.Point(546, 297);
+            this.DTP_LostDate.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.DTP_LostDate.Name = "DTP_LostDate";
             this.DTP_LostDate.Size = new System.Drawing.Size(145, 20);
             this.DTP_LostDate.TabIndex = 14;
@@ -180,7 +180,7 @@
             // 
             // LostItemForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.DTP_LostDate);
@@ -196,7 +196,7 @@
             this.Controls.Add(this.LB_Catagory);
             this.Controls.Add(this.LB_ItemName);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "LostItemForm";
             this.Text = "LostItemForm";
             this.Load += new System.EventHandler(this.LostItemForm_Load);
--- lostandfound.cs/Program.cs
+++ lostandfound.cs/Program.cs
@@ -14,12 +14,12 @@ namespace lostandfound.cs
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Login());
-            //Application.Run(new Dashboard());
+            Application.Run(new Dashboard("Sujoy"));
             //Application.Run(new Register());
             //Application.Run(new LostItemForm());
             //Application.Run(new FoundItemForm());
             //Application.Run(new SearchItemForm());
-            Application.Run(new ViewReports());
+            //Application.Run(new ViewReports());
 
         }
     }
