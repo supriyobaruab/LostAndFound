dee01ca5db2301c704b411ad80105245a881aaef
Sujoy Barua <sujoybinary@gmail.com>
2026-08-26 09:55:55 +0600

    Fixed All the possible  Bugs for both Admin and Staff Dashbords - Yet Some Fixes are to be taken for setting up auto generated id for each items

--- lostandfound.cs/Program.cs
+++ lostandfound.cs/Program.cs
@@ -13,12 +13,12 @@ namespace lostandfound.cs
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
+            Application.Run(new Login());
             //Application.Run(new Dashboard("Supriyo","24-59312-3"));
             //Application.Run(new Register());
             // Application.Run(new LostItemForm());
             //Application.Run(new FoundItemForm());
-             Application.Run(new StaffDashboard());
+            //Application.Run(new StaffDashboard());
             //Application.Run(new SearchItemForm());
             //Application.Run(new ViewReports());
             //Application.Run(new AdminDashboard());
